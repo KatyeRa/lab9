d91bd70ccd2d1688a3cf68bad6d3b3f0c1d71fbc
KatyeRa <74654685+KatyeRa@users.noreply.github.com>
2021-01-28 15:02:16 +0300

    Add files via upload

--- 2.js
+++ 2.js
@@ -1,9 +1,11 @@
-let arr = [1, 2, 3, 4, 5, 6];
+let arr = [1, 2, 3, 4, 5, 6]
 
 function Name(arr) {
-  len = arr.lengh;
-  for(let i = 0; i < len - 1; i++){
+  const Names = arr.lengh
+  for(let i = 0; i < Names - 1; i++) {
     console.log(arr.pop() + "осталось" + arr)
   }
+    console.log(arr[0])
 }
-Name(arr);
+
+Name(arr)
